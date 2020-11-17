0875c64fc3ab5bbce49a2d3f0a22a2d43bd96104
Leo Wang <leo.wang142001@gmail.com>
2020-11-17 12:56:36 -0500

    added exception handling and commands

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,22 +1,27 @@
-from discord.ext.commands import Bot as BaseBot
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
+from asyncio import sleep
 from datetime import datetime
 
+from discord.ext.commands import Bot as BaseBot
+from discord.ext.commands import Context
+from discord.ext.commands import (CommandNotFound, BadArgument, MissingRequiredArgument)
+from discord.errors import HTTPException, Forbidden
+
+from ..helpers.getTime import get_current_time
 from ..db import db
+
 import os
 
 PREFIX = '/'
 OWNER_IDS = [164144088818515968]
 COGS = []
+IGNORE_EXCEPTIONS = (CommandNotFound, BadArgument)
 
 for filename in os.listdir('./lib/cogs'):
     if filename.endswith('.py'):
         COGS.append(f'lib.cogs.{filename[:-3]}')
-
-
-def get_current_time():
-    return datetime.now().strftime("[%H:%M:%S]")
 
 
 class Bot(BaseBot):
@@ -33,13 +38,20 @@ class Bot(BaseBot):
         with open('lib/bot/TOKEN.txt', 'r', encoding="utf-8") as token:
             self.token = token.read()
 
+        self.load_cogs()
+
         print(get_current_time(), "Bot is running")
 
         super().run(self.token, reconnect=True)
 
+    def load_cogs(self):
+        for cog in COGS:
+            self.load_extension(cog)
+            print(get_current_time(), f'Loaded cog: {cog}')
+
     async def print_message(self):
         channel = self.get_channel(636378952025374794)
-        await channel.send("Test print message")
+        await channel.send("Hourly message trigger")
 
     async def on_connect(self):
         print(get_current_time(), "Logged in as {0.user}".format(self))
@@ -53,22 +65,24 @@ class Bot(BaseBot):
         raise
 
     async def on_command_error(self, ctx, exc):
-        if isinstance(exc, CommandNotFound):
-            await ctx.send("Command not found")
-        elif hasattr(exc, 'original'):
-            raise exc.original
+        if any([isinstance(exc, error) for error in IGNORE_EXCEPTIONS]):
+            pass
+        elif isinstance(exc, MissingRequiredArgument):
+            await ctx.send("Argument(s) are missing from command")
+        elif isinstance(exc.original, HTTPException):
+            await ctx.send("Unable to send message (likely too long)")
+        elif isinstance(exc.original, Forbidden):
+            await ctx.send("I don't have permission to do that")
         else:
             raise exc
 
     async def on_ready(self):
         if not self.ready:
-            self.ready = True
             self.guild = self.get_guild(636310389797290024)
-            self.scheduler.add_job(self.print_message, CronTrigger(second="0,15,30,45"))
+            self.scheduler.add_job(self.print_message, CronTrigger(second="0", minute="0"))
             self.scheduler.start()
-            for cog in COGS:
-                self.load_extension(cog)
-                print(get_current_time(), f'Loaded cog: {cog}')
+
+            self.ready = True
             print(get_current_time(), "Bot is ready")
         else:
             print(get_current_time(), "Bot reconnected")
--- lib/cogs/tests.py
+++ lib/cogs/tests.py
@@ -1,24 +1,29 @@
+from random import randint
+
 from discord.ext.commands import Cog
 from discord.ext.commands import command
-import discord
-# from discord.ext.commands import commands
+from discord.errors import HTTPException
+from discord import Embed, Colour
 
 
 class tests(Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @Cog.listener()
+    async def on_ready(self):
+        pass
+
     @command(name="ping")
     async def ping(self, ctx):
         await ctx.send(f'Pong! {round(self.bot.latency * 1000)}ms')
-        print(f'Ping: {self.bot.letency * 1000}ms')
 
     @command(name="source")
     async def source(self, ctx):
-        embed = discord.Embed(
+        embed = Embed(
             title='Shiver Discord Bot',
             description='Open source discord bot made for the chillzone discord',
-            colour=discord.Colour.blue(),
+            colour=Colour.blue(),
             url='https://github.com/Leo-Wang-Toronto/Shiver-Bot'
         )
 
@@ -27,6 +32,22 @@ class tests(Cog):
         embed.add_field(name='Contributors:', value='From github.com/Leo-Wang-Toronto/Shiver-Bot')
         embed.add_field(name='Asi4n#4243', value='github.com/Leo-Wang-Toronto', inline=False)
         await ctx.send(embed=embed)
+
+    @command(name="roll", aliases=['dice'])
+    async def roll_dice(self, ctx, dice_type: str):
+        dice, value = [int(v) for v in dice_type.lower().split("d")]
+        if dice <= 0 or value <= 0:
+            await ctx.send("You can't roll negative values dummy")
+        elif dice > 100 or value > 100:
+            await ctx.send("Values entered too large (over 100)")
+        else:
+            rolls = [randint(1, value) for i in range(dice)]
+            await ctx.send(" + ".join([str(r) for r in rolls]) + f" = {sum(rolls)}")
+
+    @roll_dice.error
+    async def roll_dice_error(self, ctx, exc):
+        if isinstance(exc.original, HTTPException):
+            await ctx.send("Result is too large D:")
 
 
 def setup(bot):
