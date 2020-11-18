3f7c97a88f6bae0158831a2e46518bc10d0113a8
Leo Wang <leo.wang142001@gmail.com>
2020-11-18 15:17:36 -0500

    changed all timezones to est

--- lib/cogs/info.py
+++ lib/cogs/info.py
@@ -1,10 +1,16 @@
 from typing import Optional
 from datetime import datetime
+from datetime import tzinfo
+from pytz import timezone
+from pytz import utc
 
 from discord.ext.commands import Cog
 from discord.ext.commands import command
 from discord import Embed
 from discord import Member
+
+est = timezone("US/Eastern")
+utc = utc
 
 class Info(Cog):
     def __init__(self, bot):
@@ -22,16 +28,22 @@ class Info(Cog):
             title = f"{target.name}#{target.discriminator}",
             description = target.mention,
             colour = target.colour,
-            timestamp = datetime.now()
+            timestamp = datetime.now().astimezone(est)
         )
         embed.set_thumbnail(url=target.avatar_url)
+
+        # discord API doesn't return their datetimes with a timezone for some reason, even though they are in UTC by default
+        joined = target.joined_at
+        created = target.created_at
+        joined = utc.localize(joined)
+        created= utc.localize(created)
 
         fields = [
             ("ID", target.id, False),
             ("Roles", ', '.join([role.mention for role in target.roles if str(role) != "@everyone"]), True),
             ("Highest role", target.top_role.mention, False),
-            ("Join date", target.joined_at.strftime("%d/%m/%Y, %H:%M:%S"), True),
-            ("Account created", target.created_at.strftime("%d/%m/%Y, %H:%M:%S"), True),
+            ("Join date", joined.astimezone(est).strftime("%d/%m/%Y, %H:%M:%S"), True),
+            ("Account created", created.astimezone(est).strftime("%d/%m/%Y, %H:%M:%S"), True),
         ]
 
         for name, value, inline in fields:
